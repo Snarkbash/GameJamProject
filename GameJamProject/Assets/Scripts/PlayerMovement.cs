b0cf69a2058293f7df304189ebcffbee0c079e07
Jake <jakejamesnorris@gmail.com>
2021-01-25 14:20:50 +0000

    Basic movement

--- GameJamProject/Assets/Scripts/PlayerMovement.cs
+++ GameJamProject/Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public bool hello = false;
-
-    // Start is called before the first frame update
+    public NavMeshAgent playerNav;
+    public float moveSpeed = 6.0f;
     void Start()
     {
-        
+        playerNav = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                playerNav.destination = hit.point;
+            }
+        }
     }
 }
